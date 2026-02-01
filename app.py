cb59bc747ad706892864f79677b1b6ec56ba3c6c
lorranlazaro <lorranlazaro@gmail.com>
2026-02-01 01:46:09 -0300

    Inclusão de informações de contato

--- app.py
+++ app.py
@@ -182,3 +182,6 @@ with col_graf4:
         st.plotly_chart(grafico_paises, use_container_width=True)
     else:
         st.warning("Nenhum dado para exibir no gráfico de países.")
+
+st.markdown("Autor: Lorran Lázaro")
+st.markdown("E-mail: lorranfelippe81@gmail.com")
